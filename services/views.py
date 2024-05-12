d3080bf220f39e958c4a4123be5bf0a16e596b5b
delss28 <delss56@mail.ru>
2024-05-12 20:06:44 +0300

    fixtures

--- services/views.py
+++ services/views.py
@@ -2,7 +2,29 @@ from django.shortcuts import render
 
 # Create your views here.
 def services(reguest):
-    return render(reguest,'services/services.html')
+    context = {
+        'services': [
+        {'name': 'ЛОР'},
+        {'name':'Хирургия'},
+        {'name':'Гинекология'},
+        {'name':'Аллергология-иммунология'},
+        {'name':'Стоматология'},
+        {'name':'Офтальмология'},
+        {'name':'Инфекционист'},
+        {'name':'Гастроэнторология'},
+        {'name':'УЗИ диагностика'},
+        {'name':'Кардиология'},
+        {'name':'Эндокринология'},
+        {'name':'Физиотерапия'},
+        {'name':'Психиатрия'},
+        {'name':'Генетик'},
+        {'name':'Дерматология'},
+        {'name':'Неврология'},
+        {'name':'Эндоскопия'},
+        {'name':'Травматология-ортопедия'},
+        ],
+    }
+    return render(reguest,'services/services.html',context)
 
 def service(reguest):
     return render(reguest,'services/service.html')
